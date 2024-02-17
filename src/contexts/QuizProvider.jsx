62d5b4a491e197b4a5ea53399e912809445afa89
abdurrahman-abdulhakeem <abdurrahmanola21@gmail.com>
2024-02-17 11:54:14 +0100

    modified back to json fake api

--- src/contexts/QuizProvider.jsx
+++ src/contexts/QuizProvider.jsx
@@ -1,111 +1,120 @@
-import { createContext, useContext, useEffect, useReducer } from "react"
+import { createContext, useContext, useEffect, useReducer } from "react";
 
-const QuizContext = createContext()
+const QuizContext = createContext();
 
-  
-  const SECS_PER_QUESTION = 30
-  
-  const initialState = {
-    questions: [],
-  
-    // Loading, Error, Ready, Active, Finish
-    status: "ready",
-    error: "",
-    answer: null,
-    index: 0,
-    score: 0,
-    secRemain: null
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+
+  // Loading, Error, Ready, Active, Finish
+  status: "ready",
+  error: "",
+  answer: null,
+  index: 0,
+  score: 0,
+  secRemain: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "quiz/received":
+      return {
+        ...state,
+        status: "ready",
+        questions: action.payload,
+        error: "",
+      };
+
+    case "quiz/failed":
+      return {
+        ...state,
+        status: "error",
+        error: action.payload,
+      };
+
+    case "loading":
+      return {
+        ...state,
+        status: "loading",
+      };
+
+    case "active":
+      return {
+        ...state,
+        status: "active",
+        secRemain: state.questions.length * SECS_PER_QUESTION,
+      };
+
+    case "answer":
+      const option = state.questions[state.index].correctOption;
+      const point = state.questions[state.index].points;
+      return {
+        ...state,
+        answer: action.payload,
+        score: action.payload === option ? state.score + point : state.score,
+      };
+    case "nextQuestion":
+      const questionLength = state.questions.length;
+
+      return {
+        ...state,
+        index:
+          state.index + 1 !== questionLength ? state.index + 1 : state.index,
+        answer: null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finish",
+        answer: null,
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secRemain: state.secRemain - 1,
+        status: state.secRemain === 0 ? "finish" : state.status,
+      };
+
+    case "reset":
+      return {
+        ...initialState,
+        questions: state.questions,
+      };
+
+    default:
+      throw new Error("Action Unknown!");
   }
-  
-  const reducer = (state, action) => {
-  
-    switch (action.type) {
-      case "quiz/received":
-        return {
-          ...state, status: "ready",  questions: action.payload, error: ""
-        }
+};
 
-      case "quiz/failed":
-        return {
-          ...state, status: "error", error: action.payload
-        }
-      
-      case "loading":
-        return {
-          ...state, status: "loading"
-        }
+export function QuizProvider({ children }) {
+  const [
+    { questions, status, index, answer, score, secRemain, error },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
-      case "active":
-        return {
-          ...state, status: "active", secRemain: state.questions.length * SECS_PER_QUESTION
-        }
-  
-      case "answer":
-        const option = state.questions[state.index].correctOption
-        const point = state.questions[state.index].points
-        return {
-          ...state, answer: action.payload, score: action.payload === option ? state.score + point : state.score
-        }
-      case "nextQuestion":
-        const questionLength = state.questions.length
-  
-        return {
-          ...state, index: state.index + 1 !== questionLength ? state.index + 1 : state.index, answer: null
-        }
-  
-      case "finish":
-  
-        return {
-          ...state, status: "finish", answer: null
-        }
-  
-      case "tick":
-        return {
-          ...state, secRemain: state.secRemain - 1,
-          status: state.secRemain === 0 ? "finish" : state.status
-        }
-  
-      case "reset":
-        return {
-          ...initialState, questions: state.questions
-        }
-  
-  
-  
-      default:
-        throw new Error("Action Unknown!");
-    }
-  }
-  
-  
-  
-  export function QuizProvider({children}) {
+  const maxQuestions = questions.length;
+  const totalPoint = questions.reduce((prev, cur) => {
+    return prev + cur.points;
+  }, 0);
 
-    const [{ questions, status, index, answer, score, secRemain, error }, dispatch] = useReducer(reducer, initialState)
+  useEffect(() => {
+    const fetchQuiz = async () => {
+      dispatch({ type: "loading" });
 
-    const maxQuestions = questions.length
-    const totalPoint = questions.reduce((prev, cur) => {
-      return prev + cur.points
-    }, 0)
+      const res = await fetch("http://localhost:8000/questions");
+      const data = await res.json();
+      // console.log(data)
+      dispatch({ type: "quiz/received", payload: data });
+    };
 
-    useEffect(()=> {
-      const fetchQuiz = async () => {
-        dispatch({type: "loading"})
-       
-        const res = await fetch("https://8389-102-89-46-211.ngrok-free.app/questions")
-        const data = await res.json()
-        // console.log(data)
-        dispatch({type:"quiz/received", payload: data})
+    fetchQuiz();
+  }, []);
 
-    
-
-      }
-
-      fetchQuiz()
-    }, [])
-
-    return (
-      <QuizContext.Provider
+  return (
+    <QuizContext.Provider
       value={{
         questions,
         status,
@@ -117,14 +126,15 @@ const QuizContext = createContext()
         totalPoint,
         error,
 
-        dispatch
-      }}>
-        {children}
-      </QuizContext.Provider>
-    )
-  }
+        dispatch,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+}
 
 export const useQuiz = () => {
-  const state = useContext(QuizContext)
-  return state
-}
+  const state = useContext(QuizContext);
+  return state;
+};
